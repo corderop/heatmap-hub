fdbdcefc4e08a67b985291d7f23159edc6ed74ce
Pablo Cordero <pablocorderoromero@gmail.com>
2023-07-20 21:28:49 +0200

    add prototype of heatmap and dashboards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
